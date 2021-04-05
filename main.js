c1d852b0d73410376c99e91229b38a593c8a67a9
nyc0lai <nyc0lai@gmail.com>
2021-04-06 01:48:43 +0300

    finish

--- main.js
+++ main.js
@@ -145,25 +145,77 @@ console.log("");
 // 15) Rotate an array to the left 1 position
 console.log("15) Rotate an array to the left 1 position");
 
-function rotateLeft (arr) {
-    //const rotatedArray = arr;
-    console.log("Initial array:",arr.join(" "));
-    const frontItem = arr.shift();
-    arr.push(frontItem);
+const rotateLeft = (arr, counts) => {
+    console.log(arr);
+    for(let i = 0; i < counts; i++) {
+        const frontItem = arr.shift();
+        arr.push(frontItem);
+    }
     return arr;
 }
-
-console.log("Rotated to left 1 pos. :",rotateLeft(["H","E","L","L","O"]).join(" "));
+console.log(rotateLeft(["H","E","L","L","O"], 1));
 console.log("");
 
 // 16) Reverse an array
 console.log("16) Reverse an array");
 
-function reversArray (masiv) {
-    for (i = 0; i < masiv.length; i++) {
-        const revArr = rotateLeft(masiv);
-        console.log(revArr);
-        return revArr;
+const reversArray = ["H","E","L","L","O"," ","W","O","R","L","D"];
+console.log(reversArray);
+console.log(reversArray.reverse());
+console.log("");
+
+// 17) Create a function that will merge two arrays and return the result as a new array
+console.log("17) Create a function that will merge two arrays and return the result as a new array");
+
+const combineTwoArray = (word1, word2) => word1.concat(word2);
+
+console.log(combineTwoArray("Hello ", "World!"));
+console.log("");
+
+// 18) Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
+
+console.log("18) Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both");
+//varianta 1
+const compare = (item1, item2) => {
+ const newArray1 = [];
+ const newArray2 = [];
+ for(let i = 0; i < item1.length; i++){
+    if(!item2.includes(item1[i]))
+        newArray1.push(item1[i])
     }
+for(let j = 0; j < item2.length; j++){
+    if(!item1.includes(item2[j]))
+        newArray2.push(item2[j])
+    }
+  return newArray1.concat(newArray2);  
 }
-console.log("Reversed array is:",reversArray([1,2,3,4,5,6,7,8,9,10]).join(" "));
+
+//varianta 2
+const compare2 = (fisrst, second) => {
+    const newArray1 = fisrst.filter(namba => !second.includes(namba));
+    const newArray2 = second.filter(namba => !fisrst.includes(namba));
+    return newArray1.concat(newArray2);
+}
+
+console.log(compare2([1,2,3,4], [1,3,6,7,8,2]));
+console.log("");
+
+// 19) Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+console.log("19) Create a function that will receive an array of numbers as argument and will return a new array with distinct elements");
+
+const distinct = (numbers) => {
+const distinctArray = numbers.map(namba => namba*11);
+    return distinctArray;
+}
+
+console.log(distinct([1,55,8,3,14,2,7,6,4,11]));
+console.log("");
+
+// 20) Create a function that will return the number of words in a text
+console.log("20) Create a function that will return the number of words in a text");
+
+const countWords = words => words.split(" ").length;
+console.log("Create a function that will receive an array of numbers as argument and will return a new array with distinct elements");
+console.log(countWords("Create a function that will receive an array of numbers as argument and will return a new array with distinct elements"), "words");
+
+//finish, huh
