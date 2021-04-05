b70b3f112fd3dba658284ac9d9177c2c7a8bc971
nyc0lai <nyc0lai@gmail.com>
2021-04-05 17:54:44 +0300

    to 16 point

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ for(let i = 1; i <= 10; i++) {
     console.log(i);
 }
 console.log("");
+
 // 2) Print the odd numbers less than 100
 console.log("2) Print the odd numbers less than 100");
 for(let i = 100; i >= 0; i--){
@@ -12,6 +13,7 @@ for(let i = 100; i >= 0; i--){
     console.log(i);
 }
 console.log("");
+
 // 3) Print the multiplication table with 7
 console.log("3) Print the multiplication table with 7");
 
@@ -96,7 +98,7 @@ console.log ("Max. number is:",Math.max(...positiveAndNegative));
 console.log("");
 
 // 12) Create a function that will return a Boolean specifying if a number is prime
-console.log("13) Create a function that will return a Boolean specifying if a number is prime");
+console.log("12) Create a function that will return a Boolean specifying if a number is prime");
 
 function primeNumber (number) {
     if(number === 1){
@@ -112,5 +114,56 @@ function primeNumber (number) {
         }
     return true;}
 }
-console.log(primeNumber(4));
+console.log(primeNumber(5));
 
+// 13) Calculate the sum of digits of a positive integer number
+console.log("13) Calculate the sum of digits of a positive integer number");
+
+const posNegatNumbers = [-1, 0, -3.5, 4.11, 6.6, 66, -10, 13, 201];
+
+const clear = posNegatNumbers.map(number => {
+    if(number > 0 && Number.isInteger(number)) {
+    return number;
+    } else {return 0;}
+}
+);
+
+const sumResult = clear.reduce((num1, num2) => num1 + num2);
+console.log(posNegatNumbers.join("; "));
+console.log(`Sum of positive integer digits is: ${sumResult}`);
+console.log("");
+
+// 14) Print the first 100 prime numbers
+console.log("14) Print the first 100 prime numbers");
+
+for(let i = 1; i < 100; i++){
+    if (primeNumber(i))
+    console.log(i)
+}
+console.log("");
+
+// 15) Rotate an array to the left 1 position
+console.log("15) Rotate an array to the left 1 position");
+
+function rotateLeft (arr) {
+    //const rotatedArray = arr;
+    console.log("Initial array:",arr.join(" "));
+    const frontItem = arr.shift();
+    arr.push(frontItem);
+    return arr;
+}
+
+console.log("Rotated to left 1 pos. :",rotateLeft(["H","E","L","L","O"]).join(" "));
+console.log("");
+
+// 16) Reverse an array
+console.log("16) Reverse an array");
+
+function reversArray (masiv) {
+    for (i = 0; i < masiv.length; i++) {
+        const revArr = rotateLeft(masiv);
+        console.log(revArr);
+        return revArr;
+    }
+}
+console.log("Reversed array is:",reversArray([1,2,3,4,5,6,7,8,9,10]).join(" "));
